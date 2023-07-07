ca0a2936e1b5ea12eb69c2f16361a8dbb18ca18c
Laura Rumiantseva <gromina.l.d@gmail.com>
2023-07-07 10:15:33 +0600

    setup project
    add configuration files, initialize npm, app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const { PORT = 3000 } = process.env;
+const db = mongoose.connection;
+
+mongoose.connect('mongodb://127.0.0.1:27017/movies', {
+  useNewUrlParser: true,
+});
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', console.info.bind(console, 'Connected to MongoDB'));
+
+db.once('open', () => {
+  const app = express();
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(cors());
+
+  app.listen(PORT, (error) => {
+    if (error) {
+      console.error('Server failed to start:', error);
+    } else {
+      console.info('Server is running');
+    }
+  });
+});
